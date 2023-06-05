bc7349f9fc48019555ed6d238358d31e3cb46de0
nilgunnb <nilgunnb@code.edu.az>
2023-06-05 20:33:14 +0400

    uploaded

---
+++ js/app.js
@@ -0,0 +1,26 @@
+let sum = 0;
+
+function Sum(x, y) {
+    if (isNaN(x)===true || isNaN(y)===true){
+        alert('invalid number')
+    }
+    else{
+        while (x<=y){
+            sum+=x
+            x++;
+        }
+        console.log(sum);
+    }
+}
+
+Sum(10,15);
+
+
+
+function increaseDiv(){
+    let div = document.querySelector(".box")
+    let initialwidth = div.clientWidth;
+    div.style.width = initialwidth + 50 + "px";
+    let initialheight = div.clientHeight;
+    div.style.height = initialheight + 20 + "px"
+}
